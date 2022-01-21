a576363e6126de4d7c1c66da482e3f0d293d1879
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2022-01-21 18:59:49 +0000

    implementation without redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {Formik} from "formik";
+import {useState} from "react";
 
 function App() {
+
+  const [shoppingList, setShoppingList] = useState([])
+
+  const handleSubmit = (values, e) => {
+    values.id = Date.now()
+    setShoppingList(prevState => [...prevState, values])
+    e.resetForm()
+  }
+
+  const deleteShoppingList = (id) => {
+    setShoppingList(shoppingList.filter(item => item.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1 className="text-center">Shopping List</h1>
+        <Formik initialValues={{item:""}} onSubmit={handleSubmit}>
+          {
+            ({values, handleSubmit, handleChange, handleBlur}) => (
+                <form onSubmit={handleSubmit} className="custom-form">
+                  <input required={true} onBlur={handleBlur} value={values.item} onChange={handleChange} name="item"
+                         className="form-control"/>
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            )
+          }
+        </Formik>
+
+        <div>
+          {
+            shoppingList.map(item => (
+                <div key={item.id} className="item">
+                  <div>
+                    <h4>{item.item}</h4>
+                    <button type="button" className="btn btn-danger" onClick={()=> deleteShoppingList(item.id)}>Delete</button>
+                  </div>
+                </div>
+            ))
+          }
+        </div>
+      </div>
   );
 }
 
